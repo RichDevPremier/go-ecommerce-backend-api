98cc2db95dc4a5ad2357e9bc68a0e7360a24e10b
RichDevPremier <danghuugiau.it@gmail.com>
2025-11-17 10:09:47 +0700

    Test github personal

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,6 +11,7 @@ func main() {
 
   // Start server on port 8080 (default)
   // Server will listen on 0.0.0.0:8080 (localhost:8080 on Windows)
+  //For test github personal
   if err := r.Run(); err != nil {
     log.Fatalf("failed to run server: %v", err)
   }
